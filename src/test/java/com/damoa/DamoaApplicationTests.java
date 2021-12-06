36f71620d7e80acf9ef299b5f46738a85b042c8f
Ahyun Kim <zlzzlzz2l@naver.com>
2021-12-06 23:20:59 +0900

    Create: 프로젝트 초기 설정

---
+++ src/test/java/com/damoa/DamoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
