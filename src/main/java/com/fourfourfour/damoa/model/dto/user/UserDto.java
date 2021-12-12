ace85e634efa2257f66308475f940f06c163451a
Ahyun Kim <zlzzlzz2l@naver.com>
2021-12-12 20:41:03 +0900

    Update: #7 - 회원 Dto 유효성 체크 추가

--- src/main/java/com/fourfourfour/damoa/model/dto/user/UserDto.java
+++ src/main/java/com/fourfourfour/damoa/model/dto/user/UserDto.java
@@ -2,9 +2,11 @@ package com.fourfourfour.damoa.model.dto.user;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @ApiModel(value = "UserDto : 회원 정보", description = "회원 정보를 가집니다.")
@@ -16,29 +18,39 @@ public class UserDto {
 
     @ApiModelProperty(value = "식별자")
     private Long userIdx;
-    @NonNull
+
+    @Email(message = "올바른 이메일이 아닙니다.")
     @ApiModelProperty(value = "이메일")
     private String userEmail;
-    @NonNull
+
+    @NotNull(message = "필수 사항입니다.")
     @ApiModelProperty(value = "패스워드")
     private String userPw;
-    @NonNull
+
+    @NotNull(message = "필수 사항입니다.")
     @ApiModelProperty(value = "닉네임")
     private String userNickname;
+
     @ApiModelProperty(value = "성별")
     private String userGender;
+
     @ApiModelProperty(value = "생년월일")
     private LocalDate userBirthDate;
+
     @ApiModelProperty(value = "직업")
     private String userJob;
-    @NonNull
+
+    @AssertTrue(message = "필수 동의 사항입니다.")
     @ApiModelProperty(value = "서비스 약관 동의")
     private boolean userServiceAgree;
-    @NonNull
+
+    @AssertTrue(message = "필수 동의 사항입니다.")
     @ApiModelProperty(value = "개인정보 약관 동의")
     private boolean userPrivacyAgree;
+
     @ApiModelProperty(value = "위치정보 약관 동의")
     private boolean userLocationAgree;
+
     @ApiModelProperty(value = "프로모션 약관 동의")
     private boolean userPromotionAgree;
 
