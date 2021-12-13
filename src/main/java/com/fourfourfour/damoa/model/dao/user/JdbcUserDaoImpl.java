40c855f209c120ba7d4e68263a1f81fc3e0ebb86
Ahyun Kim <zlzzlzz2l@naver.com>
2021-12-13 18:44:02 +0900

    Style: #7 - 회원 Dao 쿼리문 수정
    가독성을 위해 소문자에서 대문자로 변경했습니다.

--- src/main/java/com/fourfourfour/damoa/model/dao/user/JdbcUserDaoImpl.java
+++ src/main/java/com/fourfourfour/damoa/model/dao/user/JdbcUserDaoImpl.java
@@ -66,13 +66,13 @@ public class JdbcUserDaoImpl implements UserDao {
     public void insertUser(UserDto user) {
         StringBuilder sql = new StringBuilder();
         sql
-                .append("insert into").append(" ")
+                .append("INSERT INTO").append(" ")
                 .append(TABLE_NAME).append(" ")
                 .append("(" +
                         "user_email, user_pw, user_nickname, " +
                         "user_gender, user_birth_date, user_job, " +
                         "user_service_agree, user_privacy_agree, user_location_agree, user_promotion_agree)").append(" ")
-                .append("values").append(" ")
+                .append("VALUES").append(" ")
                 .append("(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
         jdbcTemplate.update(sql.toString(),
@@ -93,9 +93,9 @@ public class JdbcUserDaoImpl implements UserDao {
     public List<String> selectEmailByEmail(String userEmail) {
         StringBuilder sql = new StringBuilder();
         sql
-                .append("select user_email from").append(" ")
+                .append("SELECT user_email FROM").append(" ")
                 .append(TABLE_NAME).append(" ")
-                .append("where user_email = ?");
+                .append("WHERE user_email = ?");
 
         List<String> resultEmail = jdbcTemplate.query(sql.toString(), new RowMapper<String>() {
             @Override
@@ -111,9 +111,9 @@ public class JdbcUserDaoImpl implements UserDao {
     public List<String> selectByNickname(String userNickname) {
         StringBuilder sql = new StringBuilder();
         sql
-                .append("select user_nickname from").append(" ")
+                .append("SELECT user_nickname FROM").append(" ")
                 .append(TABLE_NAME).append(" ")
-                .append("where user_nickname = ?");
+                .append("WHERE user_nickname = ?");
         List<String> resultNickname = jdbcTemplate.query(sql.toString(), new RowMapper<String>() {
             @Override
             public String mapRow(ResultSet rs, int rowNum) throws SQLException {
