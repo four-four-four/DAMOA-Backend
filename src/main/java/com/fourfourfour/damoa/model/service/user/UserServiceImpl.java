ef2f43f7f59dd88ae7b0139da95ba98f427afd02
Ahyun Kim <zlzzlzz2l@naver.com>
2021-12-13 22:10:04 +0900

    Refactor: #7 - 회원 서비스 이메일 & 닉네임 중복 체크 메서드 타입 변경
    회원 Dao 이메일 & 닉네임 메서드의 반환 타입이 String이므로 회원 서비스도 변경

--- src/main/java/com/fourfourfour/damoa/model/service/user/UserServiceImpl.java
+++ src/main/java/com/fourfourfour/damoa/model/service/user/UserServiceImpl.java
@@ -31,27 +31,17 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean emailDuplicationCheck(String userEmail) {
 
-        List<String> resultEmail = userDao.selectEmailByEmail(userEmail);
+        String resultEmail = userDao.selectEmailByEmail(userEmail);
 
-        if (resultEmail.isEmpty()) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return resultEmail == null;
     }
 
     @Override
     public boolean nicknameDuplicationCheck(String userNickname) {
 
-        List<String> resultNickname = userDao.selectByNickname(userNickname);
+        String resultNickname = userDao.selectByNickname(userNickname);
 
-        if (resultNickname.isEmpty()) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return resultNickname == null;
     }
 
 }
