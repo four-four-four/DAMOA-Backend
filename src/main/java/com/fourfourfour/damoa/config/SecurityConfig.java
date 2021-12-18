9718cac2009c3a1e09fb097acb95335f48746c80
김경석 <0xe82de@gmail.com>
2021-12-18 18:11:59 +0900

    Update: #9 - 스프링 시큐리티 필터 추가

---
+++ src/main/java/com/fourfourfour/damoa/config/SecurityConfig.java
@@ -0,0 +1,114 @@
+package com.fourfourfour.damoa.config;
+
+import com.fourfourfour.damoa.config.jwt.JwtAuthorizationFilter;
+import com.fourfourfour.damoa.config.jwt.JwtProperties;
+import com.fourfourfour.damoa.model.dao.user.UserDao;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+/**
+ * spring security filter(SecurityConfig 클래스)가 스프링 필터체인에 등록이 된다.
+ */
+@EnableWebSecurity
+/**
+ * secured 애노테이션 활성화, preAuthorize 애노테이션 활성화
+ */
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsFilter corsFilter;
+    private final UserDao userDao;
+    private final JwtProperties jwtProperties;
+
+    private final String jwtKey;
+    private final long tokenValidityInMilliseconds;
+
+    public SecurityConfig (
+            CorsFilter corsFilter,
+            UserDao userDao,
+            JwtProperties jwtProperties,
+            @Value("${jwt.key}") String jwtKey,
+            @Value("${jwt.token-validity-in-seconds}") long tokenValidityInMilliseconds) {
+        this.corsFilter = corsFilter;
+        this.userDao = userDao;
+        this.jwtProperties = jwtProperties;
+        this.jwtKey = jwtKey;
+        this.tokenValidityInMilliseconds = tokenValidityInMilliseconds * 1000;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                /**
+                 * csrf 비활성화
+                 * https://velog.io/@woohobi/Spring-security-csrf%EB%9E%80
+                 */
+                .csrf().disable()
+
+                /**
+                 * 세션 비활성화
+                 */
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                /**
+                 * 필터 추가
+                 */
+                .addFilter(corsFilter)
+
+                .formLogin().disable()
+
+                /**
+                 * 토큰 방식을 사용하므로
+                 * 기존의 HTTP 로그인 방식(ID, PW를 보내는 방식) 비활성화
+                 */
+                .httpBasic().disable()
+
+                /**
+                 * 로그인 필터 등록
+                 */
+                .addFilterBefore(
+                        new JwtAuthorizationFilter(
+                                authenticationManager(),
+                                userDao,
+                                jwtProperties,
+                                jwtKey,
+                                tokenValidityInMilliseconds
+                        ),
+                        UsernamePasswordAuthenticationFilter.class)
+
+                .authorizeRequests()
+
+                /**
+                 * 로그인 URI는 모든 사용자에게 허용
+                 */
+                .antMatchers("/**/api/v1/users/login").permitAll()
+                /**
+                 * 회원가입 URI는 모든 사용자에게 허용
+                 */
+                .antMatchers(HttpMethod.POST, "/api/**/users").permitAll()
+                /**
+                 * 나머지 요청은 모두 인증되어야 한다.
+                 */
+                .anyRequest().authenticated()
+        ;
+    }
+}
