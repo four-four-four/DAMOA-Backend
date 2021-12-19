3228d19fd0554c24a4c45552b5dbcfa3738b3caa
Ahyun Kim <zlzzlzz2l@naver.com>
2021-12-20 01:22:52 +0900

    Create: #10 - 회원 서비스 테스트 메서드 생성

---
+++ src/test/java/com/fourfourfour/damoa/model/service/user/UserServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.fourfourfour.damoa.model.service.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class UserServiceImplTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void isEmailDuplication() {
+    }
+
+    @Test
+    void isNicknameDuplication() {
+    }
+}
