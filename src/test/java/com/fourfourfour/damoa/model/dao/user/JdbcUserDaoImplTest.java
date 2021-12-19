dc11739734735fd85e74c3a17b9db15743808edc
Ahyun Kim <zlzzlzz2l@naver.com>
2021-12-20 01:17:52 +0900

    Update: #10 - 회원 Dao 테스트 회원가입 메서드 구현

--- src/test/java/com/fourfourfour/damoa/model/dao/user/JdbcUserDaoImplTest.java
+++ src/test/java/com/fourfourfour/damoa/model/dao/user/JdbcUserDaoImplTest.java
@@ -1,16 +1,40 @@
 package com.fourfourfour.damoa.model.dao.user;
 
 import com.fourfourfour.damoa.model.dto.user.UserDto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-
+@SpringBootTest
+@Transactional
 class JdbcUserDaoImplTest {
 
     @Autowired
     private UserDao userDao;
+
+    @Test
+    public void testInsertUser(){
+        //given
+        UserDto user = new UserDto();
+        user.setUserEmail("test@test.com");
+        user.setUserPw("1234");
+        user.setUserJob("대학생");
+        user.setUserGender("female");
+        user.setUserNickname("테스트");
+        user.setUserPromotionAgree(true);
+        user.setUserPrivacyAgree(true);
+        user.setUserLocationAgree(true);
+        user.setUserServiceAgree(true);
+        //when
+
+        userDao.insertUser(user);
+
+        //then
+        String findNickname = userDao.selectByNickname(user.getUserNickname());
+        Assertions.assertThat(user.getUserNickname()).isEqualTo(findNickname);
+    }
 
 }
