29e0fb8a2ca963b42d756c321461f9d4efa432ad
김경석 <0xe82de@gmail.com>
2022-01-18 02:20:44 +0900

    Create: 회원 엔티티 생성

---
+++ src/main/java/com/fourfourfour/damoa/api/user/entity/Member.java
@@ -0,0 +1,15 @@
+package com.fourfourfour.damoa.api.user.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+import static lombok.AccessLevel.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class Member {
+}
