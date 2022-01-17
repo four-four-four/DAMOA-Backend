2d24dcedea6b30363b4edbfca329406387594576
김경석 <0xe82de@gmail.com>
2022-01-18 02:50:39 +0900

    Create: #16 - 키워드로 수집한 데이터 엔티티 생성

---
+++ src/main/java/com/fourfourfour/damoa/api/keyword/entity/CollectedData.java
@@ -0,0 +1,19 @@
+package com.fourfourfour.damoa.api.keyword.entity;
+
+import com.fourfourfour.damoa.common.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class CollectedData extends BaseEntity {
+}
