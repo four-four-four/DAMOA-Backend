7655d5f1ceb5abcd09c9992713078f2a3e5027bf
김경석 <0xe82de@gmail.com>
2022-01-18 02:28:54 +0900

    Create: #16 - 기본 엔티티 생성

---
+++ src/main/java/com/fourfourfour/damoa/common/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.fourfourfour.damoa.common.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+
+}
