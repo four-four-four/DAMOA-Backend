f9acb84aa0776df99310ef9b5e496ae822b664d3
김경석 <0xe82de@gmail.com>
2022-01-22 10:40:23 +0900

    Update: #16 - 키워드 엔티티 업데이트

--- src/main/java/com/fourfourfour/damoa/api/keyword/entity/Keyword.java
+++ src/main/java/com/fourfourfour/damoa/api/keyword/entity/Keyword.java
@@ -1,11 +1,15 @@
 package com.fourfourfour.damoa.api.keyword.entity;
 
+import com.fourfourfour.damoa.common.entity.BaseDeletedEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static lombok.AccessLevel.PROTECTED;
 
@@ -13,6 +17,19 @@ import static lombok.AccessLevel.PROTECTED;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
+@Table(name = "tb_keyword")
 @Entity
-public class Keyword {
+public class Keyword extends BaseDeletedEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "keyword_id")
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name = "collected_data_id")
+    private List<CollectedData> collectedData = new ArrayList<>();
+
 }
