4f550f9f868e6db716f902f136698744201db422
김경석 <0xe82de@gmail.com>
2022-01-22 10:40:52 +0900

    Update: #16 - 트렌드 엔티티 업데이트

--- src/main/java/com/fourfourfour/damoa/api/trend/entity/Trend.java
+++ src/main/java/com/fourfourfour/damoa/api/trend/entity/Trend.java
@@ -1,11 +1,14 @@
 package com.fourfourfour.damoa.api.trend.entity;
 
+import com.fourfourfour.damoa.common.entity.BaseDeletedEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
 
 import static lombok.AccessLevel.PROTECTED;
 
@@ -13,6 +16,18 @@ import static lombok.AccessLevel.PROTECTED;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
+@Table(name = "tb_trend")
 @Entity
-public class Trend {
+public class Trend extends BaseDeletedEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "trend_id")
+    private Long id;
+
+    @Column(nullable = false, length = 80)
+    private String feature;
+
+    @Column(nullable = false)
+    private String data;
+
 }
