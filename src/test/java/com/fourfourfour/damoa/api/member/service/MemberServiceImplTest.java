09d85a83b0a553f587b828e620af8e9cab1c0d89
Ahyun Kim <zlzzlzz2l@naver.com>
2022-02-06 02:42:27 +0900

    Create: #21 - 회원 서비스 테스트 생성

---
+++ src/test/java/com/fourfourfour/damoa/api/member/service/MemberServiceImplTest.java
@@ -0,0 +1,15 @@
+package com.fourfourfour.damoa.api.member.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MemberServiceImplTest {
+
+    @Test
+    void isEmailDuplication() {
+    }
+
+    @Test
+    void isNicknameDuplication() {
+    }
+}
