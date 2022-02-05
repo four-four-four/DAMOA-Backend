258e87febf19aca1fae9c43efb03d58e94f1417e
Ahyun Kim <zlzzlzz2l@naver.com>
2022-02-06 02:11:05 +0900

    Refactor: #21 - 회원 등록요청 DTO 닉네임 Validation 수정

--- src/main/java/com/fourfourfour/damoa/api/member/dto/req/ReqRegisterMemberDto.java
+++ src/main/java/com/fourfourfour/damoa/api/member/dto/req/ReqRegisterMemberDto.java
@@ -16,10 +16,7 @@ public class ReqRegisterMemberDto {
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?=\\S+$).{8,20}$", message = "비밀번호를 올바르게 작성해주세요.")
     private String password;
 
-    @NotNull(message = "필수 사항입니다.")
-    @Size(min = 4, max = 10, message = "닉네임은 4자 이상 10자 이하로 입력해주세요")
-    @Pattern(regexp = "^[^ㄱ-ㅎ|ㅏ-ㅣ]*$", message = "닉네임에 자음 또는 모음을 사용할 수 없습니다.") // 자음 - 모음이 들어가면 안됨
-    @Pattern(regexp = "^[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\\s]*$", message = "닉네임에 특수문자를 사용할 수 없습니다.") // [] 안에 들어간 문자 빼고는 안됨
+    @Pattern(regexp = "^[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\\s]*$", message = "닉네임을 올바르게 작성해주세요.")
     private String nickname;
 
     private String gender;
