13cc0e1443c463973458035a679431bf63459b5e
Ahyun Kim <zlzzlzz2l@naver.com>
2022-02-06 02:51:03 +0900

    Update: #21 - 회원 서비스 테스트 닉네임 중복 체크 추가

--- src/test/java/com/fourfourfour/damoa/api/member/service/MemberServiceImplTest.java
+++ src/test/java/com/fourfourfour/damoa/api/member/service/MemberServiceImplTest.java
@@ -47,6 +47,26 @@ class MemberServiceImplTest {
     }
 
     @Test
+    @DisplayName("닉네임 중복 체크")
     void isNicknameDuplication() {
+        String nickname = "백엔드테스트";
+
+        ReqRegisterMemberDto reqMember = ReqRegisterMemberDto.builder()
+                .email("test@test.com")
+                .password(passwordEncoder.encode("Abcdefg1!"))
+                .nickname(nickname)
+                .gender("female")
+                .birthDate(LocalDate.of(1997, 10, 11))
+                .job("대학생")
+                .serviceTerm(true)
+                .privacyTerm(true)
+                .build();
+
+        boolean isNickname = memberService.isNicknameDuplication(nickname);
+        assertThat(isNickname).isFalse();
+
+        memberService.register(reqMember);
+        boolean result = memberService.isNicknameDuplication(nickname);
+        assertThat(result).isTrue();
     }
 }
