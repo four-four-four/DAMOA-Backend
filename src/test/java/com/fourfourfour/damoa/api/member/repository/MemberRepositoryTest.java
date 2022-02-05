129685bffa5fef836ff816e3754bf4ae76f87ed6
Ahyun Kim <zlzzlzz2l@naver.com>
2022-02-06 02:40:27 +0900

    Update: #21 - 회원 리포지토리 테스트 이메일 중복 체크 추가

--- src/test/java/com/fourfourfour/damoa/api/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/fourfourfour/damoa/api/member/repository/MemberRepositoryTest.java
@@ -1,13 +1,54 @@
 package com.fourfourfour.damoa.api.member.repository;
 
+import com.fourfourfour.damoa.api.member.dto.req.ReqRegisterMemberDto;
+import com.fourfourfour.damoa.api.member.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
+import static org.assertj.core.api.Assertions.*;
+
+
+@SpringBootTest
+@Transactional
 class MemberRepositoryTest {
 
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
+    @DisplayName("이메일 중복 체크")
     void existsByEmail() {
+        String memberEmail = "test@test.com";
+
+        boolean isEmail = memberRepository.existsByEmail(memberEmail);
+        assertThat(isEmail).isFalse();
+
+        memberRepository.save(Member.builder()
+                .email(memberEmail)
+                .password(passwordEncoder.encode("Abcdefg1!"))
+                .nickname("백엔드테스트")
+                .gender("female")
+                .birthDate(LocalDateTime.of(LocalDate.of(1997, 10, 11), LocalTime.now()))
+                .job("대학생")
+                .role("ROLE_member")
+                .serviceTerm(true)
+                .privacyTerm(true)
+                .build());
+
+        boolean result = memberRepository.existsByEmail(memberEmail);
+        assertThat(result).isTrue();
     }
 
     @Test
