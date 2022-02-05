cf977b03b432355b42ba5b9e0499cc744b92716e
Ahyun Kim <zlzzlzz2l@naver.com>
2022-02-06 02:32:58 +0900

    Create: #21 - 회원 리포지토리 테스트 생성

---
+++ src/test/java/com/fourfourfour/damoa/api/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.fourfourfour.damoa.api.member.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    @Test
+    void existsByEmail() {
+    }
+
+    @Test
+    void existsByNickname() {
+    }
+}
