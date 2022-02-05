dbaa3288e0ee848210876e223f40978d5a54f83e
Ahyun Kim <zlzzlzz2l@naver.com>
2022-02-06 02:48:58 +0900

    Update: #21 - 회원 서비스 테스트 이메일 중복 체크 추가

--- src/test/java/com/fourfourfour/damoa/api/member/service/MemberServiceImplTest.java
+++ src/test/java/com/fourfourfour/damoa/api/member/service/MemberServiceImplTest.java
@@ -1,12 +1,49 @@
 package com.fourfourfour.damoa.api.member.service;
 
+import com.fourfourfour.damoa.api.member.dto.req.ReqRegisterMemberDto;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
 class MemberServiceImplTest {
 
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
+    @DisplayName("이메일 중복 체크")
     void isEmailDuplication() {
+        String email = "test@test.com";
+
+        ReqRegisterMemberDto reqMember = ReqRegisterMemberDto.builder()
+                .email(email)
+                .password(passwordEncoder.encode("Abcdefg1!"))
+                .nickname("백엔드테스트")
+                .gender("female")
+                .birthDate(LocalDate.of(1997, 10, 11))
+                .job("대학생")
+                .serviceTerm(true)
+                .privacyTerm(true)
+                .build();
+
+        boolean isEmail = memberService.isEmailDuplication(email);
+        assertThat(isEmail).isFalse();
+
+        memberService.register(reqMember);
+        boolean result = memberService.isEmailDuplication(email);
+        assertThat(result).isTrue();
     }
 
     @Test
