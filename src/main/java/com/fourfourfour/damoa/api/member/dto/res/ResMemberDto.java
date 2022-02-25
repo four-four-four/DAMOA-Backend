4c2ed3d36c34339e68d120e5b451e38b38c48016
김경석 <0xe82de@gmail.com>
2022-02-26 04:03:58 +0900

    Refactor: #25 - DTO 리팩토링

--- src/main/java/com/fourfourfour/damoa/api/member/dto/res/ResMemberDto.java
+++ src/main/java/com/fourfourfour/damoa/api/member/dto/res/ResMemberDto.java
@@ -1,15 +1,16 @@
 package com.fourfourfour.damoa.api.member.dto.res;
 
+import com.fourfourfour.damoa.api.member.enums.Gender;
+import com.fourfourfour.damoa.api.member.enums.Role;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Getter
 public class ResMemberDto {
 
-    private final Long id;
+    private final Long seq;
 
     private final String email;
 
@@ -17,24 +18,23 @@ public class ResMemberDto {
 
     private final String nickname;
 
-    private final String gender;
+    private final Gender gender;
 
     private final LocalDate birthDate;
 
     private final String job;
 
-    private final String role;
+    private final Role role;
 
     @QueryProjection
-    public ResMemberDto(Long id, String email, String password, String nickname, String gender, LocalDateTime birthDate, String job, String role) {
-        this.id = id;
+    public ResMemberDto(Long seq, String email, String password, String nickname, Gender gender, LocalDate birthDate, String job, Role role) {
+        this.seq = seq;
         this.email = email;
         this.password = password;
         this.nickname = nickname;
         this.gender = gender;
-        this.birthDate = birthDate == null ? null : birthDate.toLocalDate();
+        this.birthDate = birthDate;
         this.job = job;
         this.role = role;
     }
-
 }
