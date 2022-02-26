9124065e23c44500eba2be10b92089aa7cb074ca
김경석 <0xe82de@gmail.com>
2022-02-26 10:13:22 +0900

    Refactor: #25 - 테스트 코드 리팩토링

--- src/test/java/com/fourfourfour/damoa/api/member/service/MemberServiceImplTest.java
+++ src/test/java/com/fourfourfour/damoa/api/member/service/MemberServiceImplTest.java
@@ -1,7 +1,6 @@
 package com.fourfourfour.damoa.api.member.service;
 
 import com.fourfourfour.damoa.api.member.dto.req.ReqRegisterMemberDto;
-import com.fourfourfour.damoa.api.member.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
