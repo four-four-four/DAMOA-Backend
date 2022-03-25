c9efc4a24c42a1f7cb783ef60502936f5cbfd059
Ahyun Kim <zlzzlzz2l@naver.com>
2022-03-26 02:51:13 +0900

    Update: #32 - 공지사항 등록 요청 DTO 추가
    - String 타입 title(제목)과 content(본문)을 선언하였습니다.
    - 제목과 본문은 null, "", " "을 허용하지 않기 때문에 NotBlank 어노테이션 추가하였습니다.

---
+++ src/main/java/com/fourfourfour/damoa/api/notice/dto/req/ReqNoticeDto.java
@@ -0,0 +1,24 @@
+package com.fourfourfour.damoa.api.notice.dto.req;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+public class ReqNoticeDto {
+
+    @NotBlank(message = "{blank.notice.title}")
+    private String title;
+
+    @NotBlank(message = "{blank.notice.content}")
+    private String content;
+
+    @Builder
+    public ReqNoticeDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
