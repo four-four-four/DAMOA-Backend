a8a42357e5c9781f2fd7b7707b1874b59e86c4ef
김경석 <0xe82de@gmail.com>
2022-03-31 00:40:16 +0900

    Refactor: #36 - 에러 응답 DTO 반환으로 리팩토링

--- src/main/java/com/fourfourfour/damoa/common/exception/handler/GlobalControllerAdvice.java
+++ src/main/java/com/fourfourfour/damoa/common/exception/handler/GlobalControllerAdvice.java
@@ -1,10 +1,11 @@
 package com.fourfourfour.damoa.common.exception.handler;
 
-import com.fourfourfour.damoa.common.dto.response.BaseResponseDto;
-import com.fourfourfour.damoa.common.message.ErrorMessage;
+import com.fourfourfour.damoa.common.dto.response.ErrorResponseDto;
+import com.fourfourfour.damoa.common.constant.ErrorMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
@@ -23,7 +24,7 @@ public class GlobalControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public BaseResponseDto methodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ErrorResponseDto<Map<String, Object>> methodArgumentNotValidException(MethodArgumentNotValidException e) {
         logPrint(e);
 
         Map<String, Object> data = new HashMap<>();
@@ -43,7 +44,7 @@ public class GlobalControllerAdvice {
             data.put("globalError", errorInfo);
         }
 
-        return BaseResponseDto.builder()
+        return ErrorResponseDto.<Map<String, Object>>builder()
                 .message(ErrorMessage.PATTERN)
                 .data(data)
                 .build();
@@ -51,18 +52,27 @@ public class GlobalControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
-    public BaseResponseDto illegalArgumentExceptionHandler(IllegalArgumentException e) {
+    public ErrorResponseDto<?> illegalArgumentExceptionHandler(IllegalArgumentException e) {
         logPrint(e);
-        return BaseResponseDto.builder()
+        return ErrorResponseDto.builder()
                 .message(e.getMessage())
+                .build();
+    }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(AccessDeniedException.class)
+    public ErrorResponseDto<?> accessDeniedExceptionHandler(AccessDeniedException e) {
+        logPrint(e);
+        return ErrorResponseDto.builder()
+                .message(ErrorMessage.FORBIDDEN)
                 .build();
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public BaseResponseDto exceptionHandler(Exception e) {
+    public ErrorResponseDto<?> exceptionHandler(Exception e) {
         logPrint(e);
-        return BaseResponseDto.builder()
+        return ErrorResponseDto.builder()
                 .message(ErrorMessage.ERROR)
                 .build();
     }
