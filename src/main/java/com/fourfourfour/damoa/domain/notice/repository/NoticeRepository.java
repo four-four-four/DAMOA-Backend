9ec41c7cbf0c0cb40f6198c6a1fa5dfcb343e92b
Ahyun Kim <zlzzlzz2l@naver.com>
2022-04-01 01:24:58 +0900

    Refactor: #35 - 공지사항 레포지토리 인터페이스 수정
    - null 처리를 위해 Optional 클래스로 변경하였습니다.

--- src/main/java/com/fourfourfour/damoa/domain/notice/repository/NoticeRepository.java
+++ src/main/java/com/fourfourfour/damoa/domain/notice/repository/NoticeRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
     @Query("SELECT n FROM Notice n WHERE n.seq = :seq AND n.isDeleted = FALSE")
-    Notice findBySeq(@Param("seq") Long seq);
+    Optional<Notice> findBySeq(@Param("seq") Long seq);
 }
