97a90ae03744d770af75b6f1acccab71f06dba64
Ahyun Kim <zlzzlzz2l@naver.com>
2022-04-01 01:19:07 +0900

    Create: #35 - 공지사항 댓글 서비스 구현 클래스 생성
    - 공지사항 댓글 등록 메서드 작성
    - 공지사항이 존재하지 않을 때의 예외처리 작성
    - 삭제 메서드 작성

---
+++ src/main/java/com/fourfourfour/damoa/domain/notice/service/NoticeCommentServiceImpl.java
@@ -0,0 +1,48 @@
+package com.fourfourfour.damoa.domain.notice.service;
+
+import com.fourfourfour.damoa.common.constant.ErrorMessage;
+import com.fourfourfour.damoa.domain.member.repository.MemberRepository;
+import com.fourfourfour.damoa.domain.notice.entity.Notice;
+import com.fourfourfour.damoa.domain.notice.entity.NoticeComment;
+import com.fourfourfour.damoa.domain.notice.repository.NoticeCommentRepository;
+import com.fourfourfour.damoa.domain.notice.repository.NoticeRepository;
+import com.fourfourfour.damoa.domain.notice.service.dto.NoticeCommentDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class NoticeCommentServiceImpl implements NoticeCommentService {
+
+    private final MemberRepository memberRepository;
+
+    private final NoticeRepository noticeRepository;
+
+    private final NoticeCommentRepository noticeCommentRepository;
+
+    @Override
+    @Transactional
+    public NoticeComment register(NoticeCommentDto.RegisterDto registerDto, Long memberSeq) {
+        Notice noticeSeq = noticeRepository.findBySeq(registerDto.getNoticeSeq())
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.NULL_NOTICE));
+
+        NoticeComment newComment = NoticeComment.builder()
+                .content(registerDto.getContent())
+                .writer(memberRepository.findBySeq(memberSeq).get())
+                .notice(noticeSeq)
+                .build();
+
+        return noticeCommentRepository.save(newComment);
+    }
+
+    @Override
+    @Transactional
+    public void deleteAll() {
+        noticeCommentRepository.deleteAll();
+    }
+}
