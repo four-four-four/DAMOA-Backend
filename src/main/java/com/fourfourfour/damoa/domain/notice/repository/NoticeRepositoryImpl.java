bd5c631467875542183f75325d3320ec0edeee46
Ahyun Kim <zlzzlzz2l@naver.com>
2022-04-18 19:25:55 +0900

    #43 Feat: 공지사항 커스텀 레포지토리 구현 클래스 기능 추가
    - 공지사항 카운팅 및 페이징 구현
    - 공지사항 목록 생성된 순서(내림차순)로 정렬

--- src/main/java/com/fourfourfour/damoa/domain/notice/repository/NoticeRepositoryImpl.java
+++ src/main/java/com/fourfourfour/damoa/domain/notice/repository/NoticeRepositoryImpl.java
@@ -1,5 +1,53 @@
 package com.fourfourfour.damoa.domain.notice.repository;
 
-public class NoticeRepositoryImpl {
+import com.fourfourfour.damoa.domain.notice.service.dto.NoticeResponseDto;
+import com.fourfourfour.damoa.domain.notice.service.dto.QNoticeResponseDto_ForPage;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Pageable;
 
+import javax.persistence.EntityManager;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.fourfourfour.damoa.domain.notice.entity.QNotice.notice;
+
+public class NoticeRepositoryImpl implements NoticeRepositoryCustom{
+    private final JPAQueryFactory queryFactory;
+
+    public NoticeRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public Integer countAllNotice() {
+        return queryFactory
+                .select(notice.count().intValue())
+                .from(notice)
+                .where(notice.isDeleted.isFalse())
+                .fetchOne();
+    }
+
+    @Override
+    public Optional<List<NoticeResponseDto.ForPage>> findNoticeForPage(Pageable pageable) {
+        List<NoticeResponseDto.ForPage> noticeList = queryFactory
+                .select(new QNoticeResponseDto_ForPage(
+                        notice.seq,
+                        notice.title,
+                        notice.views,
+                        notice.writer.nickname
+                ))
+                .from(notice)
+                .where(notice.isDeleted.isFalse())
+                .orderBy(notice.createdDate.desc())
+                .offset((long) (pageable.getPageNumber() - 1) * pageable.getPageSize())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        if (noticeList.size() == 0) {
+            noticeList = null;
+        }
+
+        return Optional.ofNullable(noticeList);
+    }
 }
