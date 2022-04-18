6015af069ca11b4341c87b0d410c0c954968213d
Ahyun Kim <zlzzlzz2l@naver.com>
2022-04-18 18:51:04 +0900

    #43 Feat: 공지사항 목록 조회 컨트롤러 추가

--- src/main/java/com/fourfourfour/damoa/domain/notice/controller/NoticeController.java
+++ src/main/java/com/fourfourfour/damoa/domain/notice/controller/NoticeController.java
@@ -4,8 +4,10 @@ import com.fourfourfour.damoa.domain.notice.controller.dto.NoticeRequestDto;
 import com.fourfourfour.damoa.domain.notice.service.NoticeService;
 import com.fourfourfour.damoa.common.dto.response.BaseResponseDto;
 import com.fourfourfour.damoa.common.util.AuthenticationUtil;
+import com.fourfourfour.damoa.domain.notice.service.dto.NoticeResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -37,5 +39,19 @@ public class NoticeController {
         noticeService.register(registerDto.toServiceDto(), memberSeq);
 
         return BaseResponseDto.builder().build();
+    }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_MEMBER')")
+    @ResponseStatus(OK)
+    @GetMapping
+    public BaseResponseDto<NoticeResponseDto.NoticeListPage> viewListPage(Pageable pageable) {
+
+        log.info("페이징 정보 = {}", pageable);
+
+        NoticeResponseDto.NoticeListPage noticeListPage = noticeService.getPage(pageable);
+
+        return BaseResponseDto.<NoticeResponseDto.NoticeListPage>builder()
+                .data(noticeListPage)
+                .build();
     }
 }
