fbbf768dedf82c1c1236a81187c9901c0fc01c90
Ahyun Kim <zlzzlzz2l@naver.com>
2022-04-18 19:18:39 +0900

    #43 Feat: 공지사항 커스텀 레포지토리 메서드 선언부 추가

--- src/main/java/com/fourfourfour/damoa/domain/notice/repository/NoticeRepositoryCustom.java
+++ src/main/java/com/fourfourfour/damoa/domain/notice/repository/NoticeRepositoryCustom.java
@@ -1,5 +1,13 @@
 package com.fourfourfour.damoa.domain.notice.repository;
 
-public interface NoticeRepositoryCustom {
+import com.fourfourfour.damoa.domain.notice.service.dto.NoticeResponseDto;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+import java.util.Optional;
+
+public interface NoticeRepositoryCustom {
+    Integer countAllNotice();
+
+    Optional<List<NoticeResponseDto.ForPage>> findNoticeForPage(Pageable pageable);
 }
