3c9ab155261a7722748c452440cefc358c208c3a
김경석 <0xe82de@gmail.com>
2022-04-23 11:38:39 +0900

    #52 Feat: 회원 등록 키워드 목록 조회 API 추가

--- src/main/java/com/fourfourfour/damoa/domain/member/controller/MemberController.java
+++ src/main/java/com/fourfourfour/damoa/domain/member/controller/MemberController.java
@@ -1,5 +1,8 @@
 package com.fourfourfour.damoa.domain.member.controller;
 
+import com.fourfourfour.damoa.common.util.AuthenticationUtil;
+import com.fourfourfour.damoa.domain.keyword.service.KeywordService;
+import com.fourfourfour.damoa.domain.keyword.service.dto.KeywordDto;
 import com.fourfourfour.damoa.domain.member.controller.dto.MemberRequestDto;
 import com.fourfourfour.damoa.domain.member.service.dto.MemberResponseDto;
 import com.fourfourfour.damoa.common.constant.ErrorMessage;
@@ -11,8 +14,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -22,7 +29,11 @@ import static org.springframework.http.HttpStatus.*;
 @RequestMapping("/api/v1/members")
 public class MemberController {
 
+    private final AuthenticationUtil authenticationUtil;
+
     private final MemberService memberService;
+
+    private final KeywordService keywordService;
 
     @ResponseStatus(CREATED)
     @PostMapping
@@ -60,7 +71,7 @@ public class MemberController {
     }
 
     @GetMapping("/email-duplicate-check/{email}")
-    public ResponseEntity<BaseResponseDto<?>> checkEmailDuplicate(@PathVariable String email) {
+    public ResponseEntity<BaseResponseDto<Void>> checkEmailDuplicate(@PathVariable String email) {
         log.info("이메일 중복 확인 [{}]", email);
 
         String emailChk = "^[a-zA-Z0-9]([._-]?[a-zA-Z0-9])*@[a-zA-Z0-9]([-_.]?[a-zA-Z0-9])*.[a-zA-Z]$";
@@ -75,11 +86,11 @@ public class MemberController {
             status = NO_CONTENT;
         }
 
-        return new ResponseEntity<>(BaseResponseDto.builder().build(), status);
+        return new ResponseEntity<>(BaseResponseDto.<Void>builder().build(), status);
     }
 
     @GetMapping("/nickname-duplicate-check/{nickname}")
-    public ResponseEntity<BaseResponseDto<?>> checkNicknameDuplicate(@PathVariable String nickname) {
+    public ResponseEntity<BaseResponseDto<Void>> checkNicknameDuplicate(@PathVariable String nickname) {
         log.info("닉네임 중복 확인 [{}]", nickname);
 
         String nicknameChk = "^[0-9|a-z|A-Z|가-힣|\\s]{4,10}$";
@@ -94,6 +105,21 @@ public class MemberController {
             status = NO_CONTENT;
         }
 
-        return new ResponseEntity<>(BaseResponseDto.builder().build(), status);
+        return new ResponseEntity<>(BaseResponseDto.<Void>builder().build(), status);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_MEMBER')")
+    @GetMapping("/{memberSeq}/keywords")
+    public ResponseEntity<BaseResponseDto<List<KeywordDto.BasicDto>>> memberKeywordList(@PathVariable("memberSeq") Long memberSeq,
+                                                                                        Authentication authentication) {
+
+        authenticationUtil.verifyMemberSeq(authentication, memberSeq);
+
+        List<KeywordDto.BasicDto> keywordBasicList = keywordService.keywordBasicList(memberSeq);
+        HttpStatus status = keywordBasicList == null ? NO_CONTENT : OK;
+
+        return new ResponseEntity<>(BaseResponseDto.<List<KeywordDto.BasicDto>>builder()
+                .data(keywordBasicList)
+                .build(), status);
     }
 }
