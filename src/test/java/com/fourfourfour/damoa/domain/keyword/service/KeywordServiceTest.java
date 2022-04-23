10e25c5787c2c83ff42f8597cc67deb4516fba13
김경석 <0xe82de@gmail.com>
2022-04-23 11:59:21 +0900

    #52 Test: 회원 키워드 목록 반환 서비스 메서드 테스트 코드 추가

--- src/test/java/com/fourfourfour/damoa/domain/keyword/service/KeywordServiceTest.java
+++ src/test/java/com/fourfourfour/damoa/domain/keyword/service/KeywordServiceTest.java
@@ -1,10 +1,17 @@
 package com.fourfourfour.damoa.domain.keyword.service;
 
+import com.fourfourfour.damoa.common.constant.ErrorMessage;
+import com.fourfourfour.damoa.domain.keyword.entity.Keyword;
+import com.fourfourfour.damoa.domain.keyword.entity.MemberKeyword;
 import com.fourfourfour.damoa.domain.keyword.repository.KeywordRepository;
 import com.fourfourfour.damoa.domain.keyword.repository.MemberKeywordRepository;
+import com.fourfourfour.damoa.domain.keyword.service.dto.KeywordDto;
 import com.fourfourfour.damoa.domain.member.controller.dto.MemberRequestDto;
+import com.fourfourfour.damoa.domain.member.entity.Member;
 import com.fourfourfour.damoa.domain.member.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +19,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
@@ -52,5 +63,16 @@ class KeywordServiceTest {
                 .serviceTerm(true)
                 .privacyTerm(true)
                 .build();
+    }
+
+    @Test
+    @DisplayName("회원 등록 키워드 목록 조회 - 예외 처리 : 회원 엔티티가 없는 경우")
+    void findMemberKeywordListWhenNullMember() {
+        /**
+         * 예외 처리 검증
+         */
+        assertThatThrownBy(() -> keywordService.keywordBasicList(1L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NULL_MEMBER);
     }
 }
