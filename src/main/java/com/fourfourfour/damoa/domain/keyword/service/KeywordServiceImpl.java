ef5d60898b5e7b6584706e6bf429fa0dfda6d4b8
김경석 <0xe82de@gmail.com>
2022-04-23 11:37:58 +0900

    #52 Feat: 회원이 등록한 키워드 목록 반환 메서드 추가

--- src/main/java/com/fourfourfour/damoa/domain/keyword/service/KeywordServiceImpl.java
+++ src/main/java/com/fourfourfour/damoa/domain/keyword/service/KeywordServiceImpl.java
@@ -1,12 +1,19 @@
 package com.fourfourfour.damoa.domain.keyword.service;
 
+import com.fourfourfour.damoa.common.constant.ErrorMessage;
+import com.fourfourfour.damoa.domain.keyword.entity.MemberKeyword;
 import com.fourfourfour.damoa.domain.keyword.repository.KeywordRepository;
 import com.fourfourfour.damoa.domain.keyword.repository.MemberKeywordRepository;
+import com.fourfourfour.damoa.domain.keyword.service.dto.KeywordDto;
+import com.fourfourfour.damoa.domain.member.entity.Member;
 import com.fourfourfour.damoa.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Transactional(readOnly = true)
@@ -19,4 +26,26 @@ public class KeywordServiceImpl implements KeywordService {
     private final MemberRepository memberRepository;
 
     private final KeywordRepository keywordRepository;
+
+    @Override
+    public List<KeywordDto.BasicDto> keywordBasicList(Long memberSeq) {
+        Member findMember = memberRepository.findBySeq(memberSeq)
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.NULL_MEMBER));
+
+        List<MemberKeyword> memberKeywords = memberKeywordRepository.findAllByMember(findMember)
+                .orElse(null);
+
+        if (memberKeywords == null || memberKeywords.size() == 0)
+            return null;
+        else
+            return memberKeywords.stream()
+                    .map(memberKeyword -> memberKeyword.getKeyword().toBasicDto())
+                    .collect(Collectors.toList());
+    }
+
+    @Transactional
+    @Override
+    public void deleteAll() {
+        keywordRepository.deleteAll();
+    }
 }
