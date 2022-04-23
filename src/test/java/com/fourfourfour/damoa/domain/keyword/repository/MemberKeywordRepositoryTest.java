1146791ea4d59961c5f64ba6e33150e384f2638a
김경석 <0xe82de@gmail.com>
2022-04-23 11:41:15 +0900

    #52 Test: 회원 엔티티 목록 조회 레파지토리 메서드 테스트 코드 추가

--- src/test/java/com/fourfourfour/damoa/domain/keyword/repository/MemberKeywordRepositoryTest.java
+++ src/test/java/com/fourfourfour/damoa/domain/keyword/repository/MemberKeywordRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.fourfourfour.damoa.domain.keyword.repository;
 
 import com.fourfourfour.damoa.domain.keyword.entity.Keyword;
+import com.fourfourfour.damoa.domain.keyword.entity.MemberKeyword;
 import com.fourfourfour.damoa.domain.member.entity.Member;
 import com.fourfourfour.damoa.domain.member.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,6 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -61,5 +67,23 @@ class MemberKeywordRepositoryTest {
                 .serviceTerm(true)
                 .privacyTerm(true)
                 .build();
+    }
+
+    @Test
+    @DisplayName("회원 엔티티로 회원 키워드 엔티티 목록 조회 - 키워드가 없는 경우")
+    void findMemberKeywordListNonExistByMember() {
+        /**
+         * 테스트용 데이터 생성
+         */
+        Member savedMember = memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        /**
+         * 회원이 등록한 키워드가 없을 경우 사이즈가 0인 리스트가 반환된다.
+         */
+        List<MemberKeyword> findMemberKeywords = memberKeywordRepository.findAllByMember(savedMember)
+                .orElse(null);
+        assertThat(findMemberKeywords.size()).isEqualTo(0);
     }
 }
