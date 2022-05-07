4393f1bfd7237a6485dd14419966f8df2642cf52
김경석 <0xe82de@gmail.com>
2022-05-07 09:07:06 +0900

    #56 Refactor: 인증 유틸 클래스 리팩토링
    SecurityContext 에서 Authentication 객체를 뽑아서 쓰도록 리팩토링하였습니다.

--- src/main/java/com/fourfourfour/damoa/common/util/AuthenticationUtil.java
+++ src/main/java/com/fourfourfour/damoa/common/util/AuthenticationUtil.java
@@ -3,21 +3,21 @@ package com.fourfourfour.damoa.common.util;
 import com.fourfourfour.damoa.common.auth.PrincipalDetails;
 import com.fourfourfour.damoa.common.constant.ErrorMessage;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthenticationUtil {
 
-    public void verifyMemberSeq(Authentication authentication, Long memberSeq) {
-        if (((PrincipalDetails) authentication.getDetails()).getMember().getSeq().equals(memberSeq)) {
+    public void verifyMember(Long memberSeq) {
+        if (((PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getDetails()).getMember().getSeq().equals(memberSeq)) {
             return;
         }
 
         throw new AccessDeniedException(ErrorMessage.ERROR);
     }
 
-    public Long getMemberSeq(Authentication authentication) {
-        return ((PrincipalDetails) authentication.getDetails()).getMember().getSeq();
+    public Long getMemberSeq() {
+        return ((PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getDetails()).getMember().getSeq();
     }
 }
