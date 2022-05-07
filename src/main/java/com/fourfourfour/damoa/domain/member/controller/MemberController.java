e95db1ab590146c5355e9f0a86774ee66729a577
김경석 <0xe82de@gmail.com>
2022-05-07 09:08:34 +0900

    #56 Refactor: 회원 컨트롤러 리팩토링
    - 로깅 삭제 : 추후에 AOP로 로깅을 대체합니다.
    - AuthenticationUtil 리팩토링에 따라 사용 중인 메서드를 리팩토링하였습니다.

--- src/main/java/com/fourfourfour/damoa/domain/member/controller/MemberController.java
+++ src/main/java/com/fourfourfour/damoa/domain/member/controller/MemberController.java
@@ -1,21 +1,20 @@
 package com.fourfourfour.damoa.domain.member.controller;
 
+import com.fourfourfour.damoa.common.constant.ErrorMessage;
+import com.fourfourfour.damoa.common.constant.Message;
+import com.fourfourfour.damoa.common.dto.response.BaseResponseDto;
 import com.fourfourfour.damoa.common.util.AuthenticationUtil;
+import com.fourfourfour.damoa.common.util.JwtTokenUtil;
 import com.fourfourfour.damoa.domain.keyword.service.KeywordService;
 import com.fourfourfour.damoa.domain.keyword.service.dto.KeywordDto;
 import com.fourfourfour.damoa.domain.member.controller.dto.MemberRequestDto;
-import com.fourfourfour.damoa.domain.member.service.dto.MemberResponseDto;
-import com.fourfourfour.damoa.common.constant.ErrorMessage;
-import com.fourfourfour.damoa.common.constant.Message;
-import com.fourfourfour.damoa.common.util.JwtTokenUtil;
-import com.fourfourfour.damoa.common.dto.response.BaseResponseDto;
 import com.fourfourfour.damoa.domain.member.service.MemberService;
+import com.fourfourfour.damoa.domain.member.service.dto.MemberResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,8 +37,6 @@ public class MemberController {
     @ResponseStatus(CREATED)
     @PostMapping
     public BaseResponseDto<?> emailRegister(final @Validated @RequestBody MemberRequestDto.RegisterDto registerDto) {
-        log.info("회원가입 정보 = {}", registerDto);
-
         if (memberService.isEmailDuplication(registerDto.getEmail())) {
             throw new IllegalArgumentException(Message.DUPLICATE_MEMBER_EMAIL);
         } else if (memberService.isNicknameDuplication(registerDto.getNickname())) {
@@ -53,13 +50,11 @@ public class MemberController {
 
     @PostMapping("/login")
     protected ResponseEntity<BaseResponseDto<MemberResponseDto.LoginInfo>> login(@RequestBody MemberRequestDto.LoginDto loginDto) {
-        log.info("로그인 정보 = {}", loginDto);
-
         HttpStatus status = null;
         MemberResponseDto.LoginInfo memberInfo = null;
         if (memberService.login(loginDto.getEmail(), loginDto.getPassword())) {
             memberInfo = memberService.getLoginDtoByEmail(loginDto.getEmail());
-            memberInfo.addJwtToken(JwtTokenUtil.getToken(loginDto.getEmail()));
+            memberInfo.addJwtToken(JwtTokenUtil.getToken(memberInfo.getSeq(), memberInfo.getEmail()));
             status = OK;
         } else {
             status = NO_CONTENT;
@@ -72,8 +67,6 @@ public class MemberController {
 
     @GetMapping("/email-duplicate-check/{email}")
     public ResponseEntity<BaseResponseDto<Void>> checkEmailDuplicate(@PathVariable String email) {
-        log.info("이메일 중복 확인 [{}]", email);
-
         String emailChk = "^[a-zA-Z0-9]([-_.]?[a-zA-Z0-9])*@[a-zA-Z0-9]([-_.]?[a-zA-Z0-9])*\\.[a-zA-Z]{2,3}$";
         if (!email.matches(emailChk)) {
             throw new IllegalArgumentException(ErrorMessage.PATTERN_MEMBER_EMAIL);
@@ -91,8 +84,6 @@ public class MemberController {
 
     @GetMapping("/nickname-duplicate-check/{nickname}")
     public ResponseEntity<BaseResponseDto<Void>> checkNicknameDuplicate(@PathVariable String nickname) {
-        log.info("닉네임 중복 확인 [{}]", nickname);
-
         String nicknameChk = "^[0-9|a-z|A-Z|가-힣|\\s]{4,10}$";
         if (!nickname.matches(nicknameChk)) {
             throw new IllegalArgumentException(ErrorMessage.PATTERN_MEMBER_NICKNAME);
@@ -110,10 +101,8 @@ public class MemberController {
 
     @PreAuthorize("hasAnyRole('ROLE_MEMBER')")
     @GetMapping("/{memberSeq}/keywords")
-    public ResponseEntity<BaseResponseDto<List<KeywordDto.BasicDto>>> memberKeywordList(@PathVariable("memberSeq") Long memberSeq,
-                                                                                        Authentication authentication) {
-
-        authenticationUtil.verifyMemberSeq(authentication, memberSeq);
+    public ResponseEntity<BaseResponseDto<List<KeywordDto.BasicDto>>> memberKeywordList(@PathVariable("memberSeq") Long memberSeq) {
+        authenticationUtil.verifyMember(memberSeq);
 
         List<KeywordDto.BasicDto> keywordBasicList = keywordService.keywordBasicList(memberSeq);
         HttpStatus status = keywordBasicList == null ? NO_CONTENT : OK;
