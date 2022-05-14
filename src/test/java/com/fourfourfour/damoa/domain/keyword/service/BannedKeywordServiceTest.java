2e041647476419c7d86e23c16fe8e8c18ca1cd33
김경석 <0xe82de@gmail.com>
2022-05-14 11:25:11 +0900

    #60 Test: 차단 여부 확인 서비스 메서드 테스트 케이스 추가

--- src/test/java/com/fourfourfour/damoa/domain/keyword/service/BannedKeywordServiceTest.java
+++ src/test/java/com/fourfourfour/damoa/domain/keyword/service/BannedKeywordServiceTest.java
@@ -5,11 +5,15 @@ import com.fourfourfour.damoa.domain.keyword.repository.BannedKeywordRepository;
 import com.fourfourfour.damoa.domain.keyword.repository.KeywordRepository;
 import com.fourfourfour.damoa.domain.member.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -47,5 +51,20 @@ class BannedKeywordServiceTest {
         bannedKeyword1 = BannedKeyword.builder()
                 .name(keywordName1)
                 .build();
+    }
+
+    @Test
+    @DisplayName("차단 여부 확인 - 차단된 경우")
+    void checkBannedKeywordTrue() {
+        // given
+        BannedKeyword savedBannedKeyword1 = bannedKeywordRepository.save(bannedKeyword1);
+        em.flush();
+        em.clear();
+
+        // when
+        boolean banned = bannedKeywordService.isBanned(savedBannedKeyword1.getName());
+
+        // then
+        assertThat(banned).isTrue();
     }
 }
