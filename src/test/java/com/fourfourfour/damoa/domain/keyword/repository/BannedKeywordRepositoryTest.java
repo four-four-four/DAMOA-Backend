0d02e858d05f47fe4fa85b289ef264e231ad6d37
김경석 <0xe82de@gmail.com>
2022-05-14 11:21:48 +0900

    #60 Test: 차단 키워드 확인 레포지토리 메서드 테스트 케이스 추가

--- src/test/java/com/fourfourfour/damoa/domain/keyword/repository/BannedKeywordRepositoryTest.java
+++ src/test/java/com/fourfourfour/damoa/domain/keyword/repository/BannedKeywordRepositoryTest.java
@@ -3,11 +3,15 @@ package com.fourfourfour.damoa.domain.keyword.repository;
 import com.fourfourfour.damoa.domain.keyword.entity.BannedKeyword;
 import com.fourfourfour.damoa.domain.member.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -38,5 +42,20 @@ class BannedKeywordRepositoryTest {
         bannedKeyword1 = BannedKeyword.builder()
                 .name("차단 키워드1")
                 .build();
+    }
+
+    @Test
+    @DisplayName("차단 키워드 확인 - 차단된 경우")
+    void checkBannedKeywordWhenBanned() {
+        // given
+        BannedKeyword savedBannedKeyword = bannedKeywordRepository.save(bannedKeyword1);
+        em.flush();
+        em.clear();
+
+        // when
+        boolean exists = bannedKeywordRepository.existsByName(savedBannedKeyword.getName());
+
+        // then
+        assertThat(exists).isTrue();
     }
 }
