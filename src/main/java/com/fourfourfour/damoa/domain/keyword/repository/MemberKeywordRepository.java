25a4ac58f4a2145c1cf324b64c1b246180afb9f9
김경석 <0xe82de@gmail.com>
2022-05-14 11:20:13 +0900

    #60 Feat: 회원, 키워드 엔티티로 회원 키워드 엔티티 조회 레포지토리 메서드 추가

--- src/main/java/com/fourfourfour/damoa/domain/keyword/repository/MemberKeywordRepository.java
+++ src/main/java/com/fourfourfour/damoa/domain/keyword/repository/MemberKeywordRepository.java
@@ -1,5 +1,6 @@
 package com.fourfourfour.damoa.domain.keyword.repository;
 
+import com.fourfourfour.damoa.domain.keyword.entity.Keyword;
 import com.fourfourfour.damoa.domain.keyword.entity.MemberKeyword;
 import com.fourfourfour.damoa.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface MemberKeywordRepository extends JpaRepository<MemberKeyword, Long> {
 
     Optional<List<MemberKeyword>> findAllByMember(Member member);
+
+    Optional<MemberKeyword> findByMemberAndKeyword(Member member, Keyword keyword);
 }
