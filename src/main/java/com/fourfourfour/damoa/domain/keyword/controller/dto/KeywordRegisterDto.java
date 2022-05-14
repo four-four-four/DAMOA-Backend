a25ae10f8c0ea3cf486970c1a6e4e965cb15bb44
김경석 <0xe82de@gmail.com>
2022-05-14 11:13:53 +0900

    #60 Feat: 필드 추가

--- src/main/java/com/fourfourfour/damoa/domain/keyword/controller/dto/KeywordRegisterDto.java
+++ src/main/java/com/fourfourfour/damoa/domain/keyword/controller/dto/KeywordRegisterDto.java
@@ -2,8 +2,12 @@ package com.fourfourfour.damoa.domain.keyword.controller.dto;
 
 import lombok.Getter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
-@ToString(of = {})
+@ToString(of = {"keywordName"})
 public class KeywordRegisterDto {
+
+    @Length(message = "{pattern}", min = 1, max = 20)
+    private String keywordName;
 }
