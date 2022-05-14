d0eecfba4169232c7130d23e2a895824850cc7b7
김경석 <0xe82de@gmail.com>
2022-05-14 11:17:34 +0900

    #60 Feat: 키워드 이름으로 데이터 존재 확인 레포지토리 메서드 추가

--- src/main/java/com/fourfourfour/damoa/domain/keyword/repository/BannedKeywordRepository.java
+++ src/main/java/com/fourfourfour/damoa/domain/keyword/repository/BannedKeywordRepository.java
@@ -4,4 +4,6 @@ import com.fourfourfour.damoa.domain.keyword.entity.BannedKeyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BannedKeywordRepository extends JpaRepository<BannedKeyword, Long> {
+
+    boolean existsByName(String name);
 }
