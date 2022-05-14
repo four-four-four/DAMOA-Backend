4251938ff0fe3f6cdf373bf5d3755e01e6c017b9
김경석 <0xe82de@gmail.com>
2022-05-14 11:19:28 +0900

    #60 Feat: 회원 키워드 등록 서비스 메서드 추가

--- src/main/java/com/fourfourfour/damoa/domain/keyword/service/MemberKeywordService.java
+++ src/main/java/com/fourfourfour/damoa/domain/keyword/service/MemberKeywordService.java
@@ -1,4 +1,8 @@
 package com.fourfourfour.damoa.domain.keyword.service;
 
+import com.fourfourfour.damoa.domain.keyword.entity.MemberKeyword;
+
 public interface MemberKeywordService {
+
+    MemberKeyword register(String keywordName, Long memberSeq);
 }
