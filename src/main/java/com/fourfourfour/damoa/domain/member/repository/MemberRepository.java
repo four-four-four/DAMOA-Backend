ab27510706a645f079f5f6a2036d4b9acf279736
김경석 <0xe82de@gmail.com>
2022-05-14 11:20:35 +0900

    #60 Refactor: 삭제 처리된 회원 데이터는 반환되지 않도록 리팩토링

--- src/main/java/com/fourfourfour/damoa/domain/member/repository/MemberRepository.java
+++ src/main/java/com/fourfourfour/damoa/domain/member/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.fourfourfour.damoa.domain.member.repository;
 
 import com.fourfourfour.damoa.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -13,5 +15,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
 
-    Optional<Member> findBySeq(Long seq);
+    @Query("SELECT m FROM Member m WHERE m.seq = :seq AND m.isDeleted = FALSE")
+    Optional<Member> findBySeq(@Param("seq") Long seq);
 }
