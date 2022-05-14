d1a2209b381254f33aadb50669306bf33d9353dd
김경석 <0xe82de@gmail.com>
2022-05-14 11:19:57 +0900

    #60 Feat: 키워드 이름으로 키워드 엔티티 조회 레포지토리 메서드 추가

--- src/main/java/com/fourfourfour/damoa/domain/keyword/repository/KeywordRepository.java
+++ src/main/java/com/fourfourfour/damoa/domain/keyword/repository/KeywordRepository.java
@@ -2,6 +2,13 @@ package com.fourfourfour.damoa.domain.keyword.repository;
 
 import com.fourfourfour.damoa.domain.keyword.entity.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+
+    @Query("SELECT k FROM Keyword k WHERE k.name = :name AND k.isDeleted = FALSE")
+    Optional<Keyword> findByName(@Param("name") String name);
 }
