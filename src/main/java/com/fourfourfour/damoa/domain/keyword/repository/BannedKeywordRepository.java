e86478adc0fb7d66d7be1a1ebe558901c268ff8c
김경석 <0xe82de@gmail.com>
2022-05-14 11:16:57 +0900

    #60 Create: BannedKeyword 레포지토리 인터페이스 생성

---
+++ src/main/java/com/fourfourfour/damoa/domain/keyword/repository/BannedKeywordRepository.java
@@ -0,0 +1,7 @@
+package com.fourfourfour.damoa.domain.keyword.repository;
+
+import com.fourfourfour.damoa.domain.keyword.entity.BannedKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BannedKeywordRepository extends JpaRepository<BannedKeyword, Long> {
+}
