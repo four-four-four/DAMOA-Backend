a3c6ea62df5d13b41b8a2e7b0aa417e3a1a7c7a1
Ahyun Kim <zlzzlzz2l@naver.com>
2022-05-28 10:41:25 +0900

    #64 Feat: 공지사항 댓글 컨트롤러 댓글 목록 조회 추가

--- src/main/java/com/fourfourfour/damoa/domain/notice/controller/NoticeCommentController.java
+++ src/main/java/com/fourfourfour/damoa/domain/notice/controller/NoticeCommentController.java
@@ -4,13 +4,17 @@ import com.fourfourfour.damoa.common.dto.response.BaseResponseDto;
 import com.fourfourfour.damoa.common.util.AuthenticationUtil;
 import com.fourfourfour.damoa.domain.notice.controller.dto.NoticeCommentRequestDto;
 import com.fourfourfour.damoa.domain.notice.service.NoticeCommentService;
+import com.fourfourfour.damoa.domain.notice.service.dto.NoticeCommentDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @Slf4j
 @RestController
@@ -29,5 +33,16 @@ public class NoticeCommentController {
         noticeCommentService.register(registerDto.toServiceDto(noticeSeq), memberSeq);
 
         return BaseResponseDto.<Void>builder().build();
+    }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_MEMBER')")
+    @ResponseStatus(OK)
+    @GetMapping
+    public BaseResponseDto<List<NoticeCommentDto.Detail>> view(@PathVariable Long noticeSeq) {
+        List<NoticeCommentDto.Detail> comments = noticeCommentService.getDetail(noticeSeq);
+
+        return BaseResponseDto.<List<NoticeCommentDto.Detail>>builder()
+                .data(comments)
+                .build();
     }
 }
