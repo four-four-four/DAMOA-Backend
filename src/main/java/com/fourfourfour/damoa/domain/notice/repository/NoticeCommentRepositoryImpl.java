be7082ddc0ae14b97900e675e9dc9ba86c0088e6
Ahyun Kim <zlzzlzz2l@naver.com>
2022-05-28 10:52:36 +0900

    #64 Feat: 공지사항 댓글 커스텀 레포지토리 구현 추가

---
+++ src/main/java/com/fourfourfour/damoa/domain/notice/repository/NoticeCommentRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.fourfourfour.damoa.domain.notice.repository;
+
+import com.fourfourfour.damoa.domain.notice.service.dto.NoticeCommentDto;
+import com.fourfourfour.damoa.domain.notice.service.dto.QNoticeCommentDto_Detail;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+import static com.fourfourfour.damoa.domain.notice.entity.QNoticeComment.*;
+
+public class NoticeCommentRepositoryImpl implements NoticeCommentRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+
+    public NoticeCommentRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public Optional<List<NoticeCommentDto.Detail>> findCommentsByNoticeSeq(Long noticeSeq) {
+        List<NoticeCommentDto.Detail> commentList = queryFactory
+                .select(new QNoticeCommentDto_Detail(
+                        noticeComment.seq,
+                        noticeComment.content,
+                        noticeComment.createdDate,
+                        noticeComment.writer.seq,
+                        noticeComment.writer.nickname
+                ))
+                .from(noticeComment)
+                .where(noticeComment.notice.seq.eq(noticeSeq).and(noticeComment.isDeleted.isFalse()))
+                .orderBy(
+                        noticeComment.createdDate.asc()
+                ).fetch();
+
+        if (commentList.size() == 0) {
+            commentList = null;
+        }
+
+        return Optional.ofNullable(commentList);
+    }
+}
