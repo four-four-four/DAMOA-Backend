f358603011f2d372f6fff91aab69d8435ac7c290
Ahyun Kim <zlzzlzz2l@naver.com>
2022-05-28 10:44:20 +0900

    #64 Feat: 공지사항 댓글 서비스 댓글 조회 선언부 추가

--- src/main/java/com/fourfourfour/damoa/domain/notice/service/NoticeCommentService.java
+++ src/main/java/com/fourfourfour/damoa/domain/notice/service/NoticeCommentService.java
@@ -3,9 +3,13 @@ package com.fourfourfour.damoa.domain.notice.service;
 import com.fourfourfour.damoa.domain.notice.entity.NoticeComment;
 import com.fourfourfour.damoa.domain.notice.service.dto.NoticeCommentDto;
 
+import java.util.List;
+
 public interface NoticeCommentService {
 
     NoticeComment register(NoticeCommentDto.RegisterDto registerDto, Long memberSeq);
+
+    List<NoticeCommentDto.Detail> getDetail(Long noticeSeq);
 
     void deleteAll();
 }
